cbf2ba7ccb0a667b427b9e0054e90b5d8abf8d1f
Abdul Ali <alinawrozie@gmail.com>
2026-08-06 00:32:01 +0100

    feat(eks): add EKS cluster module with managed node group and IRSA

---
+++ terraform/eks.tf
@@ -0,0 +1,19 @@
+module "eks" {
+    source = "terraform-aws-modules/eks/aws"
+    version = "~> 20.0"
+    cluster_name = var.cluster_name
+    
+    vpc_id = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets
+    enable_irsa = true
+    cluster_endpoint_public_access = true
+    cluster_endpoint_private_access = false
+    eks_managed_node_groups = {
+        default = {
+            min_size = 1
+            max_size = 3
+            desired_size = 2
+            instance_types = ["t3.medium"]
+        }
+    }
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
-  name = "${var.cluster_name}-vpc"
-  cidr = "10.0.0.0/16"
+  name    = "${var.cluster_name}-vpc"
+  cidr    = "10.0.0.0/16"
 
-  azs = ["${var.aws_region}a", "${var.aws_region}b"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -12,12 +12,12 @@ module "vpc" {
   single_nat_gateway = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                    = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
-  
+
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
